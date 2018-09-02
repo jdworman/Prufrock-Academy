e7f07bf6a3ce5f77969b13ca507c8f9b2e477318
Joseph Worman <jdworman84@gmail.com>
2018-09-02 13:04:39 -0400

    add carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,62 +15,91 @@
 //= require turbolinks
 //= require_tree .
 
+$(function() {
 
- 
-// CAROUSEL OBJECT
-function Carousel(containerID) {
-	this.container = document.getElementById(containerID) || document.body;
-	this.slides = this.container.querySelectorAll('.carousel');
-	this.total = this.slides.length - 1;
-	this.current = 0;
-	
-	// start on slide 1
-	this.slide(this.current);
-}
-// NEXT
-Carousel.prototype.next = function (interval) {
-	(this.current === this.total) ? this.current = 0 : this.current += 1;
-	
-	this.stop();	
-	this.slide(this.current);
-	
-	if(typeof interval === 'number' && (interval % 1) === 0) {
-		var context = this;
-		this.run = setTimeout(function() {
-			context.next(interval);
-		}, interval);
-	}
-};
-// PREVIOUS
-Carousel.prototype.prev = function (interval) {	
-	(this.current === 0) ? this.current = this.total : this.current -= 1;
-		
-	this.stop();	
-	this.slide(this.current);
-	
-	if(typeof interval === 'number' && (interval % 1) === 0) {
-		var context = this;
-		this.run = setTimeout(function() {
-			context.prev(interval);
-		}, interval);
-	}
-};
-// STOP PLAYING
-Carousel.prototype.stop = function () {
-	clearTimeout(this.run);
-};
-// SELECT SLIDE
-Carousel.prototype.slide = function (index) {	
-	if (index >= 0 && index <= this.total) { 
-		this.stop();
-		for (var s = 0; s <= this.total; s++) {
-			if (s === index) {
-				this.slides[s].style.display = "inline-block"; 
-			} else {
-				this.slides[s].style.display = 'none';
-			}
-		}
-	} else {
-		alert("Index " + index + " doesn't exist. Available : 0 - " + this.total);
-	}
-};
+    // slider type
+    $t = "slide"; // opitions are fade and slide
+    
+  	//variables
+    $f = 1000,  // fade in/out speed
+    $s = 1000,  // slide transition speed (for sliding carousel)
+    $d = 5000;  // duration per slide
+    
+    $n = $('.slide').length; //number of slides
+    $w = $('.slide').width(); // slide width
+  	$c = $('.container').width(); // container width
+   	$ss = $n * $w; // slideshow width
+  
+  	
+      function timer() {
+        $('.timer').animate({"width":$w}, $d);
+        $('.timer').animate({"width":0}, 0);
+    }
+
+  
+  // fading function
+    function fadeInOut() {
+      timer();
+        $i = 0;    
+        var setCSS = {
+            'position' : 'absolute',
+            'top' : '0',
+            'left' : '0'
+        }        
+        
+        $('.slide').css(setCSS);
+        
+        //show first item
+        $('.slide').eq($i).show();
+        
+
+        setInterval(function() {
+          timer();
+            $('.slide').eq($i).fadeOut($f);
+            if ($i == $n - 1) {
+                $i = 0;
+            } else {
+                $i++;
+            }
+            $('.slide').eq($i).fadeIn($f, function() {
+                $('.timer').css({'width' : '0'});
+            });
+
+        }, $d);
+        
+    }
+    
+    function slide() {
+      timer();
+        var setSlideCSS = {
+            'float' : 'left',
+            'display' : 'inline-block',
+          	'width' : $c
+        }
+        var setSlideShowCSS = {
+            'width' : $ss // set width of slideshow container
+        }
+        $('.slide').css(setSlideCSS);
+        $('.slideshow').css(setSlideShowCSS); 
+        
+        
+        setInterval(function() {
+            timer();
+            $('.slideshow').animate({"left": -$w}, $s, function(){
+                // to create infinite loop
+                $('.slideshow').css('left',0).append( $('.slide:first'));
+            });
+        }, $d);
+        
+    }
+    
+    if ($t == "fade") {
+        fadeInOut();
+        
+    } if ($t == "slide") {
+        slide();
+        
+    } else {
+      
+    }
+});
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,12 +1,5 @@
 class Instructor < ApplicationRecord
 
-
-  belongs_to :cohort
-
-
-  validates :first_name, :last_name, :education, presence: true
-  validates :education, inclusion: { in: Instructor.education }
-  validates :age, inclusion: { in: 0...100 }
-  validates :salary, numericality: { greater_than_or_equal_to: 0 }
+ 
 
 end
