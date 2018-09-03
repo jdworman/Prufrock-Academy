b67b6deaef2e2e25b6fe0e912c3047ae1d5ccdaa
Joseph Worman <jdworman84@gmail.com>
2018-09-03 18:33:13 -0400

    massive improvements

--- db/schema.rb
+++ db/schema.rb
@@ -51,21 +51,14 @@ ActiveRecord::Schema.define(version: 2018_09_01_220553) do
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string "first_name", limit: 25
-    t.string "last_name", limit: 50
-    t.string "email", default: "", null: false
-    t.string "password"
-=======
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
->>>>>>> 5b7a3220a35caa0032c2928ffe1200c2708e1335
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
