7a5ac11818635799f444b6ba312f316954c30fa1
Joseph Worman <jdworman84@gmail.com>
2018-09-05 10:14:20 -0400

    changed controllers around

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -8,6 +8,7 @@ class CohortsController < ApplicationController
   end
 
   def new
+    @cohorts = Cohort.new
   end
 
   def show
@@ -15,32 +16,31 @@ class CohortsController < ApplicationController
   end
 
   def create
-    @cohorts = Cohort.new(params.require(:cohorts).permit(:first_name, :last_name, :age, :education))
-  if @cohorts.save
-    redirect_to @cohorts
-  else
-    errors = @cohorts.errors.full_messages
-    flash.now[:error] = errors
-    render 'new'
-  end
-
-  def update
-    @cohorts = Cohort.find(params[:id])
-
-    if @cohorts.update(params.require(:cohort).permit(:first_name, :last_name, :email, :age, :education))
+    @cohorts = Cohort.new(params.require(:cohort).permit(:name, :start_date, :end_date, :student_id, :instructor_id))
+    if @cohorts.save
       redirect_to @cohorts
     else
-      render 'edit'
+      errors = @cohorts.errors.full_messages
+      flash.now[:error] = errors
+      render 'new'
     end
   end
 
-  def destroy
-    @cohorts = Cohort.find(params[:id])
-    @cohorts.destroy
-    respond_to do |format|
-      format.html { redirect_to @cohorts }
+    def update
+      @cohorts = Cohort.find(params[:id])
 
+      if @cohorts.update(params.require(:cohort).permit(:name, :start_date, :end_date, :student_id, :instructor_id))
+        redirect_to @cohorts
+      else
+        render 'edit'
+      end
     end
-  end
 
+    def destroy
+      @cohorts = Cohort.find(params[:id])
+      @cohorts.destroy
+      respond_to do |format|
+        format.html { redirect_to @cohorts }
+      end
+    end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,37 +8,39 @@ class CoursesController < ApplicationController
   end
 
   def new
+    @courses = Course.new
   end
 
   def show
-    @courses = Course.find(params[:id]) 
+    @courses = Course.find(params[:id])
   end
 
   def create
-    @courses = Course.new(params.require(:courses).permit(:first_name, :last_name, :email, :age, :education))
- 
-    @courses.save
-    redirect_to @courses
-  end
-
-  def update
-    @courses = Course.find(params[:id])
-
-    if @courses.update(params.require(:course).permit(:first_name, :last_name, :email, :age, :education))
+    @courses = Course.new(params.require(:course).permit(:name, :hours))
+    if @courses.save
       redirect_to @courses
     else
-      render 'edit'
+      errors = @courses.errors.full_messages
+      flash.now[:error] = errors
+      render 'new'
     end
   end
 
-  def destroy
-    @courses = Course.find(params[:id])
-    @courses.destroy
-    respond_to do |format|
-      format.html { redirect_to @courses }
-       
+    def update
+      @courses = Course.find(params[:id])
+
+      if @courses.update(params.require(:course).permit(:name, :hours))
+        redirect_to @courses
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @courses = Course.find(params[:id])
+      @courses.destroy
+      respond_to do |format|
+        format.html { redirect_to @courses }
+      end
     end
   end
-
-end
-
