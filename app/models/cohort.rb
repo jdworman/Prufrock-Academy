5ad563e108d4fb8dfd841cbd6e716b5a7b9bfea6
Joseph Worman <jdworman84@gmail.com>
2018-09-10 17:34:45 -0400

    join table

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,9 @@
 class Cohort < ApplicationRecord
+  belongs_to :course
+    has_one :instructor
+    has_many :cohort_students
+    has_many :students, through: :cohort_students
+
   validates :name, length: {minimum: 2, maximum: 16}
   validates :start_date,  presence: true
   validates :end_date,  presence: true
